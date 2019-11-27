104159adc5300dff5e73c81e194ed95eef4e5557
unknown <puddingcat945@gmail.com>
2019-11-27 21:26:08 +0800

    update

--- app.py
+++ app.py
@@ -43,10 +43,14 @@ def handle_message(event):
     msg = event.message.text
     r = '妳好漂亮'
 
-    if msg == 'hi' :
-        r = 'hi'
+    if msg in ['hi', 'Hi'] :
+        r = '嗨'
     elif msg == '你吃飯了嗎' :
         r = '等你請我'
+    elif msg == '你是誰' :
+        r = '我是讚喔機器人'
+    elif '訂位' in msg :
+        r = '算我一份 謝謝'
 
     line_bot_api.reply_message(
         event.reply_token,
